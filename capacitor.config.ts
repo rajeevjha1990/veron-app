b0053efc61c0a6462f23ab2c79d41dd7b0e7eb08
Rajeev Jha <jharajeev374@yahoo.com>
2025-07-21 07:24:27 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'veromoneyApp',
+  webDir: 'www'
+};
+
+export default config;
